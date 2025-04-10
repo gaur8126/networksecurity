e1aaff914a0acf606892bc13d7de0cf27291f035
gaur8126 <gaurlokesh1211@gmail.com>
2025-04-10 18:12:31 +0530

    model trainer completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
 from networksecurity.exception.execption import NetworkSecurityException
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -33,6 +34,13 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")
         print(data_transformation_artifact)
+
+        ## Model Trainer 
+        logging.info("Model training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Trainig completed")
         
 
     except Exception as e:
--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -70,12 +70,12 @@ class DataTransformation:
             ## training dataframe
             input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN],axis=1)
             target_feature_train_df = train_df[TARGET_COLUMN]
-            target_feature_train_df = target_feature_train_df.replace(-1,1)
+            target_feature_train_df = target_feature_train_df.replace(-1,0)
 
             ## test dataframe 
             input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN],axis=1)
             target_feature_test_df = test_df[TARGET_COLUMN]
-            target_feature_test_df = target_feature_test_df.replace(-1,1)
+            target_feature_test_df = target_feature_test_df.replace(-1,0)
 
             preprocessor = self.get_data_transformer_object()
 
